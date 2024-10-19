86e16181ddfcf119364da33819b58ea9716060bc
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-10-19 13:10:46 -0400

    board class semi done, process made

--- chess_game/chess_game.ino
+++ chess_game/chess_game.ino
@@ -31,7 +31,7 @@ enum PieceType {
 
 // The Piece Class
 class Piece {
-  private:
+  public:
     // Piece type:
     PieceType type;
     // Color of the piece:
@@ -42,8 +42,6 @@ class Piece {
     // Pawn: Double move flag - 1 if this pawn can move two squares
     bool double_move;
 
-
-  public:
     PieceType get_type() {
       return type;
     }
@@ -67,6 +65,8 @@ class Piece {
     // This move function does not check for any potential checks that might occur by this move.
     // The checks should be done by the board class - the board checks the board state after possible moves and evaluate if the move is legal
     std::vector<std::pair<int, int>> get_possible_moves(Board board) {
+      // TODO: WE COULD RETURN A vector<pair<pair,pair>> The first pair is destination, second is the piece that is taken
+      // TODO: can change this to a "board pointer" if we don't like passing the board as an argument
       std::vector<std::pair<int, int>> moves;
       // switch piece type
       switch (type) {
@@ -77,9 +77,9 @@ class Piece {
             for(int j = x-1; j <= x+1; j++){
               if (i == y && j == x) continue;
               // If empty piece, or opponent piece, you can move there
-              if (board.pieces[i][j].get_color() != color) {
+              if (board.pieces[i][j]->get_color() != color) {
                 moves.push_back(std::make_pair(j, i));
-              } else if (board.pieces[i][j].get_type() == EMPTY) {
+              } else if (board.pieces[i][j]->get_type() == EMPTY) {
                 moves.push_back(std::make_pair(j, i));
               }
             }
@@ -88,26 +88,26 @@ class Piece {
           if (color == 0) {
             if (board.white_king_castle) {
               // Check if squares between king and rook are empty
-              if (board.pieces[5][0].get_type() == EMPTY && board.pieces[6][0].get_type() == EMPTY) {
+              if (board.pieces[0][5]->get_type() == EMPTY && board.pieces[0][6]->get_type() == EMPTY) {
                 moves.push_back(std::make_pair(6, 0));
               }
             }
             if (board.white_queen_castle) {
               // Check if squares between king and rook are empty
-              if (board.pieces[1][0].get_type() == EMPTY && board.pieces[2][0].get_type() == EMPTY && board.pieces[3][0].get_type() == EMPTY) {
+              if (board.pieces[0][1]->get_type() == EMPTY && board.pieces[0][2]->get_type() == EMPTY && board.pieces[0][3]->get_type() == EMPTY) {
                 moves.push_back(std::make_pair(2, 0));
               }
             }
           } else {
             if (board.black_king_castle) {
               // Check if squares between king and rook are empty
-              if (board.pieces[5][7].get_type() == EMPTY && board.pieces[6][7].get_type() == EMPTY) {
+              if (board.pieces[7][5]->get_type() == EMPTY && board.pieces[7][6]->get_type() == EMPTY) {
                 moves.push_back(std::make_pair(6, 7));
               }
             }
             if (board.black_queen_castle) {
               // Check if squares between king and rook are empty
-              if (board.pieces[1][7].get_type() == EMPTY && board.pieces[2][7].get_type() == EMPTY && board.pieces[3][7].get_type() == EMPTY) {
+              if (board.pieces[7][1]->get_type() == EMPTY && board.pieces[7][2]->get_type() == EMPTY && board.pieces[7][3]->get_type() == EMPTY) {
                 moves.push_back(std::make_pair(2, 7));
               }
             }
@@ -123,9 +123,9 @@ class Piece {
               int y_loop = y+dy;
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
-                if (board.pieces[x_loop][y_loop].get_type() == EMPTY) {
+                if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
                   moves.push_back(std::make_pair(x_loop, y_loop));
-                } else if (board.pieces[x_loop][y_loop].get_color() != color) {
+                } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
                   moves.push_back(std::make_pair(x_loop, y_loop));
                   break;
@@ -150,9 +150,9 @@ class Piece {
               int y_loop = y+dy;
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
-                if (board.pieces[x_loop][y_loop].get_type() == EMPTY) {
+                if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
                   moves.push_back(std::make_pair(x_loop, y_loop));
-                } else if (board.pieces[x_loop][y_loop].get_color() != color) {
+                } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
                   moves.push_back(std::make_pair(x_loop, y_loop));
                   break;
@@ -171,22 +171,22 @@ class Piece {
           // TODO: Implement knight moves
           // 8 possible moves - don't add if edge of board / own piece
           // Knight moves logic
-int dx[] = {2, 2, -2, -2, 1, 1, -1, -1};
-int dy[] = {1, -1, 1, -1, 2, -2, 2, -2};
+          int dx[] = {2, 2, -2, -2, 1, 1, -1, -1};
+          int dy[] = {1, -1, 1, -1, 2, -2, 2, -2};
 
-// Loop through all possible knight moves
-for (int i = 0; i < 8; i++) {
-    int newX = x + dx[i];
-    int newY = y + dy[i];
-    
-    // Check if the move is within bounds of the board
-    if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
-        // If the square is empty or contains an opponent's piece
-        if (board.pieces[newY][newX].get_type() == EMPTY || board.pieces[newY][newX].get_color() != color) {
-            moves.push_back(std::make_pair(newX, newY));
-        }
-    }
-}
+          // Loop through all possible knight moves
+          for (int i = 0; i < 8; i++) {
+              int newX = x + dx[i];
+              int newY = y + dy[i];
+              
+              // Check if the move is within bounds of the board
+              if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
+                  // If the square is empty or contains an opponent's piece
+                  if (board.pieces[newY][newX]->get_type() == EMPTY || board.pieces[newY][newX]->get_color() != color) {
+                      moves.push_back(std::make_pair(newX, newY));
+                  }
+              }
+          }
         break;
         case ROOK:
           // 4 possible directions - don't add if edge of board / own piece
@@ -199,9 +199,9 @@ for (int i = 0; i < 8; i++) {
               int y_loop = y+dy;
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
-                if (board.pieces[x_loop][y_loop].get_type() == EMPTY) {
+                if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
                   moves.push_back(std::make_pair(x_loop, y_loop));
-                } else if (board.pieces[x_loop][y_loop].get_color() != color) {
+                } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
                   moves.push_back(std::make_pair(x_loop, y_loop));
                   break;
@@ -228,59 +228,6 @@ for (int i = 0; i < 8; i++) {
       return moves;
     }
 
-    // Function that moves the piece to a new square
-    // Assuming all moves are legal
-    // Update king_castle, queen_castle, en_passant, double_move flags
-    void move(int new_x, int new_y, Board board) {
-      // TODO: migrate this function to board class (because we are updating the pieces themselves and the board grid, might be better to do it there)
-      if (type == PAWN) {
-        // If the pawn moves two squares, it can be taken en passant
-        if (abs(new_y - y) == 2) {
-          board.en_passant_square_x = new_x;
-          board.en_passant_square_y = new_y;
-          // TODO: Board stores the only pawn that is en-passantable, and update the board object and this piece object
-          // After each move, board checks that piece that just moved and see if that's en-passantable and stores it
-        }
-        // If the pawn moves, it can't move two squares
-        double_move = false;
-
-        // TODO: if we take en passant, remove the piece that was taken
-      }
-      // If the king moves, it can't castle
-      if (type == KING) {
-        if (color == 0) {
-          board.white_king_castle = false;
-          board.white_queen_castle = false;
-        } else {
-          board.black_king_castle = false;
-          board.black_queen_castle = false;
-        }
-
-        // TODO: Check if the king is moving two squares, then it's castling
-        // If it's castling, move the rook (call board[0][0].move(3, 0, board)) basically
-      }
-      // If rook moves, king can't castle that side
-      if (type == ROOK) {
-        if (color == 0) {
-          // a1 rook - white queen side
-          if (x == 0) {
-            board.white_queen_castle = false;
-          } else {
-            board.white_king_castle = false;
-          }
-        } else {
-          // a8 rook - black queen side
-          if (x == 0) {
-            board.black_queen_castle = false;
-          } else {
-            board.black_king_castle = false;
-          }
-        }
-      }
-      x = new_x;
-      y = new_y;
-    }
-
     // Constructor
     Piece(PieceType new_type, bool new_color, int new_x, int new_y) {
       type = new_type;
@@ -291,24 +238,146 @@ for (int i = 0; i < 8; i++) {
     }
 };
 
+// The Board Class
+class Board {
+  public:
+    // 8x8 array of pieces (pointer to a piece)
+    Piece* pieces[8][8];
+    // Castling flags
+    bool black_king_castle;
+    bool black_queen_castle;
+    bool white_king_castle;
+    bool white_queen_castle;
+    // En passant square (The location of the pawn that CAN be taken, not the location that the enemy pawn takes it from...)
+    int en_passant_square_x;
+    int en_passant_square_y;
+    // Move counter 
+    int draw_move_counter;
 
-typedef struct Square {
-  int x;
-  int y;
-}
+    // For ease of checking if a king is in check, we store the location of the kings
+    // Black king location
+    int black_king_x;
+    int black_king_y;
+    // White king location
+    int white_king_x;
+    int white_king_y;
 
-typedef struct Board {
-  bool white_king_castle;
-  bool white_queen_castle;
-  bool black_king_castle;
-  bool black_queen_castle;
-  int half_move_counter;
-  int full_move_counter;
-  Squre en_passant_square; // (-1, -1) for no square
-  Piece board_pieces[8][8];
-} Board
+    // Function that moves a piece
+    // We are given the original x, original y, new x, new y, if capture happens, a "capture x" and "capture y"
+    // In the end of the function, each individual piece's x and y should be updated
+    // Also, the board array will reflect the new state of the board
+    void move_piece(int x, int y, int new_x, int new_y, int capture_x, int capture_y) {
+      // By default no en passant square
+      en_passant_square_x = -1;
+      en_passant_square_y = -1;
+      // PAWN:
+      if (pieces[y][x]->get_type() == PAWN) {
+        // If the pawn moves two squares, it can be taken en passant
+        if (abs(new_y - y) == 2) {
+          en_passant_square_x = new_x;
+          en_passant_square_y = new_y;
+        }
+        // If the pawn moves, it can't move two squares
+        pieces[y][x]->double_move = false;
+      }
+      // If the king moves, it can't castle
+      if (pieces[y][x]->type == KING) {
+        if (pieces[y][x]->color == 0) {
+          white_king_castle = false;
+          white_queen_castle = false;
+          // Update the king's location
+          white_king_x = new_x;
+          white_king_y = new_y;
+        } else {
+          black_king_castle = false;
+          black_queen_castle = false;
+          // Update the king's location
+          black_king_x = new_x;
+          black_king_y = new_y;
+        }
 
+        if (abs(new_x - x) == 2) {
+          // Castling
+          // If the king moves two squares, move the rook
+          // If the king moves to the right, move the rook to the left
+          // Since castling doesn't involve capture but 2 pieces are involved
+          // we exchange the rook with the empty space here
+          if (new_x > x) {
+            // Change the rook's x to 3
+            pieces[new_y][7]->x = new_x - 1;
+            // Change the empty space's x to 7
+            pieces[new_y][new_x - 1]->x = 7;
+            // Do a piece swap
+            Piece *temp = pieces[new_y][7];
+            pieces[new_y][7] = pieces[new_y][new_x - 1];
+            pieces[new_y][new_x - 1] = temp;
+          } else {
+            // Change the rook's x to 5
+            pieces[new_y][0]->x = new_x + 1;
+            // Change the empty space's x to 0
+            pieces[new_y][new_x + 1]->x = 0;
+            // Do a piece swap
+            Piece *temp = pieces[new_y][0];
+            pieces[new_y][0] = pieces[new_y][new_x + 1];
+            pieces[new_y][new_x + 1] = temp;
+          }
+        }
+      }
+      // If rook moves, king can't castle that side
+      if (pieces[y][x]->type == ROOK) {
+        if (pieces[y][x]->color == 0) {
+          // a1 rook - white queen side
+          if (x == 0) {
+            white_queen_castle = false;
+          } else {
+            white_king_castle = false;
+          }
+        } else {
+          // a8 rook - black queen side
+          if (x == 0) {
+            black_queen_castle = false;
+          } else {
+            black_king_castle = false;
+          }
+        }
+      }
+      // If rook is taken, king can't castle that side
+      if (capture_x != -1) {
+        // Check if the captured piece is a1, a8, h1, h8 (don't care if its a rook or not)
+        if (capture_x == 0) {
+          if (capture_y == 0) {
+            white_queen_castle = false;
+          } else if (capture_y == 7) {
+            black_queen_castle = false;
+          }
+        } else if (capture_x == 7) {
+          if (capture_y == 0) {
+            white_king_castle = false;
+          } else if (capture_y == 7) {
+            black_king_castle = false;
+          }
+        }
+      }
 
+      // Update piece's x and y (move captured piece first, then the new_x, new_y, then the original x, y)
+      // Captured (set piece type to EMPTY)
+      if (capture_x != -1) pieces[capture_y][capture_x]->type = EMPTY;
+      // New x, new y
+      // Set coordinate first, then exchange the pointers
+      pieces[new_y][new_x]->x = x; // we know the new piece is empty
+      pieces[new_y][new_x]->y = y;
+      pieces[x][y]->x = new_x;
+      pieces[x][y]->y = new_y;
+      // Do a piece swap
+      Piece *temp = pieces[new_y][new_x];
+      pieces[new_y][new_x] = pieces[y][x];
+      pieces[y][x] = temp;
+    }
+
+    // Constructor
+    Board() {
+      // TODO
+    }
 
 void setup() {
   // put your setup code here, to run once:
