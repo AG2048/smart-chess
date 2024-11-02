3062b5b62bee4a9558553bfa56df5a974817fbdc
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-11-02 12:06:50 -0400

    added extra move capture pair in the get_possible_move()

--- chess_game/chess_game.ino
+++ chess_game/chess_game.ino
@@ -64,10 +64,10 @@ class Piece {
     // Function that returns x,y coordinates of all possible moves
     // This move function does not check for any potential checks that might occur by this move.
     // The checks should be done by the board class - the board checks the board state after possible moves and evaluate if the move is legal
-    std::vector<std::pair<int, int>> get_possible_moves(Board board) {
+    std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> get_possible_moves(Board board) {
       // TODO: WE COULD RETURN A vector<pair<pair,pair>> The first pair is destination, second is the piece that is taken
       // TODO: can change this to a "board pointer" if we don't like passing the board as an argument
-      std::vector<std::pair<int, int>> moves;
+      std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> moves;
       // switch piece type
       switch (type) {
         case EMPTY:
@@ -77,11 +77,11 @@ class Piece {
             for(int j = x-1; j <= x+1; j++){
               if (i == y && j == x) continue;
               // If empty piece, or opponent piece, you can move there
-              if (board.pieces[i][j]->get_color() != color) {
-                moves.push_back(std::make_pair(j, i));
-              } else if (board.pieces[i][j]->get_type() == EMPTY) {
-                moves.push_back(std::make_pair(j, i));
-              }
+              if (board.pieces[i][j]->get_type() == EMPTY) {
+                moves.push_back(std::make_pair(std::make_pair(j, i), std::make_pair(-1, -1))); // make a pair of destination, while capture square is (-1, -1) (no capture)
+              } else if (board.pieces[i][j]->get_color() != color) {
+                moves.push_back(std::make_pair(std::make_pair(j, i), std::make_pair(j, i))); // make a pair of destination and capture square, which is the same
+              } 
             }
           }
           // Castling
@@ -89,26 +89,26 @@ class Piece {
             if (board.white_king_castle) {
               // Check if squares between king and rook are empty
               if (board.pieces[0][5]->get_type() == EMPTY && board.pieces[0][6]->get_type() == EMPTY) {
-                moves.push_back(std::make_pair(6, 0));
+                moves.push_back(std::make_pair(std::make_pair(6, 0), std::make_pair(-1, -1)));
               }
             }
             if (board.white_queen_castle) {
               // Check if squares between king and rook are empty
               if (board.pieces[0][1]->get_type() == EMPTY && board.pieces[0][2]->get_type() == EMPTY && board.pieces[0][3]->get_type() == EMPTY) {
-                moves.push_back(std::make_pair(2, 0));
+                moves.push_back(std::make_pair(std::make_pair(2, 0), std::make_pair(-1, -1)));
               }
             }
           } else {
             if (board.black_king_castle) {
               // Check if squares between king and rook are empty
               if (board.pieces[7][5]->get_type() == EMPTY && board.pieces[7][6]->get_type() == EMPTY) {
-                moves.push_back(std::make_pair(6, 7));
+                moves.push_back(std::make_pair(std::make_pair(6, 7), std::make_pair(-1, -1)));
               }
             }
             if (board.black_queen_castle) {
               // Check if squares between king and rook are empty
               if (board.pieces[7][1]->get_type() == EMPTY && board.pieces[7][2]->get_type() == EMPTY && board.pieces[7][3]->get_type() == EMPTY) {
-                moves.push_back(std::make_pair(2, 7));
+                moves.push_back(std::make_pair(std::make_pair(2, 7), std::make_pair(-1, -1)));
               }
             }
           }
@@ -124,10 +124,11 @@ class Piece {
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
                 if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  // Empty square, no capture
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(-1, -1)));
                 } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(x_loop, y_loop)));
                   break;
                 } else {  // != EMPTY, == color
                   // Found a friendly piece; don't look past it
@@ -151,10 +152,10 @@ class Piece {
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
                 if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(-1, -1)));
                 } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(x_loop, y_loop)));
                   break;
                 } else {  // != EMPTY, == color
                   // Found a friendly piece; don't look past it
@@ -182,8 +183,10 @@ class Piece {
               // Check if the move is within bounds of the board
               if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
                   // If the square is empty or contains an opponent's piece
-                  if (board.pieces[newY][newX]->get_type() == EMPTY || board.pieces[newY][newX]->get_color() != color) {
-                      moves.push_back(std::make_pair(newX, newY));
+                  if (board.pieces[newY][newX]->get_type() == EMPTY) {
+                      moves.push_back(std::make_pair(std::make_pair(newX, newY), std::make_pair(-1, -1)));
+                  } else if (board.pieces[newY][newX]->get_color() != color) {
+                      moves.push_back(std::make_pair(std::make_pair(newX, newY), std::make_pair(newX, newY)));
                   }
               }
           }
@@ -200,10 +203,10 @@ class Piece {
               while (1) {
                 if (x_loop < 0 || y_loop < 0 || x_loop > 7 || y_loop > 7) break;  // if any coordinate is OOB, stop
                 if (board.pieces[y_loop][x_loop]->get_type() == EMPTY) {
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(-1, -1)));
                 } else if (board.pieces[y_loop][x_loop]->get_color() != color) {
                   // Enemy piece encountered; we can capture it, but don't look past it
-                  moves.push_back(std::make_pair(x_loop, y_loop));
+                  moves.push_back(std::make_pair(std::make_pair(x_loop, y_loop), std::make_pair(x_loop, y_loop)));
                   break;
                 } else {  // != EMPTY, == color
                   // Found a friendly piece; don't look past it
@@ -415,7 +418,7 @@ class Board {
       // Copy the pieces
       for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-          new_board.pieces[i][j] = *pieces[i][j]; // TODO: check if this copy method is ok, since pieces doesn't have any pointers
+          new_board.pieces[i][j] = *(pieces[i][j]); // TODO: check if this copy method is ok, since pieces doesn't have any pointers
         }
       }
       // Copy the castling flags
