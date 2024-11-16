c5c4e937b487c5d17cd8dbd60d7d4234f542328b
z-huu <zach_1003@hotmail.com>
2024-11-16 13:08:52 -0500

    testing code

--- chess_game/chess_game.ino
+++ chess_game/chess_game.ino
@@ -20,34 +20,57 @@ board.en_passant_square_y
 board.move_counter
 */
 
-
-
-Board* p_board;
+Board *p_board;
 std::vector<std::pair<std::pair<int, int>, std::pair<int, int>>> all_moves[8][8];
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
   // put your setup code here, to run once:
   // Initialize board class
   p_board = new Board;
-  
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
   // Generate possible moves for all pieces in the board class
-  for (int row = 0; row < 8; row++) {
-    for (int col = 0; col < 8; col++) {
+  for (int row = 0; row < 8; row++)
+  {
+    for (int col = 0; col < 8; col++)
+    {
       all_moves[row][col] = p_board->pieces[row][col]->get_possible_moves(p_board);
+      Serial.print(row);
+      Serial.print("\t");
+      Serial.print(col);
+      Serial.print("\t");
+      Serial.print(p_board->pieces[row][col]->type);
+      Serial.println();
+      for (int i = 0; i < all_moves[row][col].size(); i++)
+      {
+        Serial.print("\t");
+        Serial.print(all_moves[row][col][i].first.first);
+        Serial.print("\t");
+        Serial.print(all_moves[row][col][i].first.second);
+        Serial.print("\t");
+        Serial.print(all_moves[row][col][i].second.first);
+        Serial.print("\t");
+        Serial.print(all_moves[row][col][i].second.second);
+      }
+      Serial.println();
     }
+    Serial.println();
   }
+
+  delay(5000);
 
   // Remove illegal moves
 
   // Detect checkmate / draw
 
-    // If detected, enter game over
+  // If detected, enter game over
 
-  // Coordinate with LEDs --> indicate possible moves, selected piece, 
+  // Coordinate with LEDs --> indicate possible moves, selected piece,
 
   // Coordinate with motors --> tell them what pieces to move
 
@@ -55,10 +78,9 @@ void loop() {
 
   // Check pawn promotion
 
-    // If pawn on end ranks, select promotion piece
+  // If pawn on end ranks, select promotion piece
 
-    // Coordinating with LEDs, motors again
+  // Coordinating with LEDs, motors again
 
-    // Update Board & Piece classes again
-
+  // Update Board & Piece classes again
 }
