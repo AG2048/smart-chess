1e829d5bdcb53941170b6d42523d2eff1d0d65aa
z-huu <zach_1003@hotmail.com>
2025-02-02 21:29:52 -0500

    add alternating idle screens, remove redunant argument in display_idle_screen

--- chess_OLED_prototype/chess_OLED_prototype.ino
+++ chess_OLED_prototype/chess_OLED_prototype.ino
@@ -16,31 +16,6 @@
 #define SCREEN_ADDRESS 0x3C // Address is 0x3D for 128x64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Important game state items (picked from chess_game.ino)
-
-enum GameState {
-  GAME_POWER_ON,    // Power on the board, motors calibrate
-  GAME_IDLE,        // No game is being played yet, waiting for a game to start
-  GAME_INITIALIZE,  // Game started, initialize the board
-  GAME_BEGIN_TURN,  // Begin a turn - generate moves, remove illegal moves,
-                    // check for checkmate/draw
-  GAME_WAIT_FOR_SELECT,  // Joystick moving, before pressing select button on
-                         // valid piece
-  GAME_WAIT_FOR_MOVE,  // Joystick moving, after pressing select button on valid
-                       // destination
-  GAME_MOVE_MOTOR,     // Motors moving pieces
-  GAME_END_MOVE,       // Update chess board, see if a pawn can promote
-  GAME_WAIT_FOR_SELECT_PAWN_PROMOTION,  // Joystick moving, before pressing
-                                        // select button on promotion piece
-  GAME_PAWN_PROMOTION_MOTOR,  // Motors moving pieces for pawn promotion
-  GAME_END_TURN,              // End a turn - switch player
-  GAME_OVER_WHITE_WIN,        // White wins
-  GAME_OVER_BLACK_WIN,        // Black wins
-  GAME_OVER_DRAW,             // Draw
-  GAME_RESET                  // Reset the game
-};
-
-GameState game_state;
 bool player_turn; // 0 for white, 1 for black
 
 /* OLED wrapper functions
@@ -49,17 +24,19 @@ bool player_turn; // 0 for white, 1 for black
 
 */
 
-static uint32_t last_interacted_time;
+static uint32_t last_interacted_time, last_idle_change;
 // Flags to ensure we only print corresponding screens ONCE, not every loop
 static bool idle_one, idle_two, displayed_player, displayed_computer;
 bool button;
 Timer OLED_timer;
 #define IDLE_ONE_DELAY_MS 1000*5 //20 seconds in ms
 #define IDLE_TWO_DELAY_MS 1000*10 //40 seconds in ms
+#define IDLE_DELAY_MS 1000*5 // 5 seconds in ms
 #define NUMFLAKES 10 // number of snowflakes to use in idle animation
 #define LOGO_WIDTH 16     // bitmap dimensions
 #define LOGO_HEIGHT 16
 
+static int8_t icons[NUMFLAKES][3]; // Used for idle animation
 #define XPOS   0 // Indexes into the 'icons' array for display_idle_screen fn
 #define YPOS   1
 #define DELTAY 2
@@ -98,36 +75,29 @@ static const unsigned char PROGMEM logo_bmp[] = // A bitmap that we can change t
 */
 
 // No screen_num argument -- assuming both screens will display the same thing while idle
-void display_idle_screen(Timer timer, bool interacted, bool select_player, bool select_computer, uint8_t comp_diff) {
+void display_idle_screen(Timer timer, bool interacted, bool screen_select, uint8_t comp_diff) {
   /* display_idle_screen arguments
   Timer timer, passed in so we can poll and see the current time and hence how much time has passed
 
   bool interacted, passed high if a user has interacted with the screen, so if necessary we know to 
   exit idle states
 
-  bool select_player, high if that is the screen to be displayed outside of idle states
-
-  bool select_computer, high if that is the screen to be displayed outside of idle states
+  bool screen_select, high if displaying select_player screen, low if displaying select_computer
 
   uint8_t comp_diff, the difficulty value the user is selecting for Stockfish
   */
   uint32_t current_time = timer.read();
-
-  static int8_t icons[NUMFLAKES][3]; /* Used for idle animation
-  [x][0] --> XPOS
-  [x][1] --> YPOS
-  [x][2] --> DELTAY */  
-
+  
   if (interacted) { // If the user has interacted with the display
-    if (idle_one) {  // Do we need to check idle_one? 
-      idle_one = 0;
-      display.stopscroll();
-    }
-    idle_two = 0; // if user has interacted, we are certainly not in an idle screen
+
+    // Setting idle screens back to default, so we can enter them again later
+    idle_one = 1;
+    idle_two = 0; 
+    display.stopscroll();
 
     // Draw the screen we're currently on
     // TO DO: find images for both screens (sel_player, sel_comp) and convert them to bitmaps 
-    if (select_player) {
+    if (screen_select) {
 
       if (!displayed_player) {
         display_select_player();
@@ -136,7 +106,7 @@ void display_idle_screen(Timer timer, bool interacted, bool select_player, bool 
       }
       displayed_computer = 0;
 
-    } else if (select_computer) {
+    } else { // If screen_select is low
 
       if (!displayed_computer) {
         display_select_computer(comp_diff);
@@ -151,7 +121,30 @@ void display_idle_screen(Timer timer, bool interacted, bool select_player, bool 
 
   } else { // If the user hasn't interacted with the display
 
-    if (current_time - last_interacted_time > IDLE_TWO_DELAY_MS) {// Showing screen two : idle animation
+    if ( current_time - last_interacted_time > IDLE_DELAY_MS ) {// Showing an idle animation
+
+      // Reset display_screen flags, so that we can display them again once we exit idle screens
+      displayed_computer = 0;
+      displayed_player = 0;
+
+      if ( current_time - last_idle_change > IDLE_DELAY_MS ) { // Swapping idle screens
+        idle_one = !idle_one;
+        idle_two = !idle_two;
+        last_idle_change = current_time;
+        display.stopscroll();
+      }
+
+      if (idle_one) { // Scrolling text
+
+        if (current_time == last_idle_change) { // If we are displaying this idle screen for the first time
+          display_idle_scroll();
+        }
+
+      } else if (idle_two) { // Idle animation
+
+        display_idle_animation(current_time);
+
+      }
 /*
 if current - last interacted > IDLE delay && current - lastIdleChange > 5s:
   // we are in idle, and it's time to change
@@ -160,63 +153,9 @@ if current - last interacted > IDLE delay && current - lastIdleChange > 5s:
   else: set idle1 to true, idle 2 to false do whatever. 
   (make sure to have a "default", that idle 1 comes first)
 */
-      displayed_computer = 0; // Reset the display screen flags
-      displayed_player = 0;
-      if (idle_one) { // Clear idle_one flag
-        idle_one = 0;
-        display.stopscroll();
-      }
-
-      // Snowflake animation from example
-      // Every time this function is called, draw the next frame of animation
-      if (!idle_two) { // We are just entering idle animation. Initialize!
-        idle_two = 1;
-        for (int8_t f = 0; f < NUMFLAKES; f++) {
-          icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-          icons[f][YPOS]   = -LOGO_HEIGHT;
-          icons[f][DELTAY] = random(1, 6);
-        }
-      } else { // We've already initialized this idle screen. Draw next frame!
-        display.clearDisplay();
-
-        // Draw each snowflake:
-        for (int8_t f = 0; f < NUMFLAKES; f++) {
-          display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, SSD1306_WHITE);
-        }
-
-        display.display(); // Show the display buffer on the screen
-        // delay(200);        // Pause for 1/10 second. Should we?
-
-        // Then update coordinates of each flake...
-        for (int8_t f = 0; f < NUMFLAKES; f++) {
-          icons[f][YPOS] += icons[f][DELTAY];
-          // If snowflake is off the bottom of the screen...
-          if (icons[f][YPOS] >= display.height()) {
-            // Reinitialize to a random position, just off the top
-            icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
-            icons[f][YPOS]   = -LOGO_HEIGHT;
-            icons[f][DELTAY] = random(1, 6);
-          }
-        }
-
-      }
-      
-    } else if (current_time - last_interacted_time > IDLE_ONE_DELAY_MS) { // Showing screen one : scrolling text
-      displayed_computer = 0; // Reset the display screen flags
-      displayed_player = 0;
-      if (!idle_one) { // We only want this to run once, when we first enter the second idle screen
-        idle_one = 1; // We need to clear this once we exit 
-        display.clearDisplay();
-        display.setTextSize(2);
-        display.println(F("  Spark"));
-        display.print(F("SMARTCHESS"));
-        display.display();
-        display.startscrollleft(0x00, 0x0F); // (row 1, row 2). 0x0F as 2nd row scrolls whole display.
-        //display.startscrollright(15, 0x0F);
-      }
-
+ 
     } else { // We're not idle, but user hasn't interacted
-      if (select_player) {
+      if (screen_select) {
 
         if (!displayed_player) {
           display_select_player();
@@ -224,7 +163,7 @@ if current - last interacted > IDLE delay && current - lastIdleChange > 5s:
         }
         displayed_computer = 0;
 
-      } else if (select_computer) {
+      } else {
 
         if (!displayed_computer) {
           display_select_computer(comp_diff);
@@ -264,6 +203,7 @@ void display_select_player() {
 void display_select_computer(uint8_t comp_diff) {
   display.clearDisplay();
   display.setTextSize(1);
+  display.setCursor(0, 0);
   display.println(F("STOCKFISH"));
   display.print(F("<Human  LV:"));
   // Draw bitmap
@@ -271,10 +211,60 @@ void display_select_computer(uint8_t comp_diff) {
   display.display();
 }
 
+void display_idle_scroll() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.println(F(" > Spark <"));
+  display.print(F("SMARTCHESS"));
+  display.display();
+  // display.startscrollleft(0, 1); // (row 1, row 2). Scrolls just the first row of text.
+  display.startscrollright(2, 3); // SSD1306 can't handle two concurrent scroll directions.
+}
+
+void display_idle_animation(uint32_t current_time) {
+
+  /* Once we make OLED_timer a global thing in our library, we won't need to pass in current_time anymore */
+
+  if (current_time == last_idle_change) { // We are just entering idle animation. Initialize!
+
+    for (int8_t f = 0; f < NUMFLAKES; f++) {
+      icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+      icons[f][YPOS]   = -LOGO_HEIGHT;
+      icons[f][DELTAY] = random(1, 6);
+    }
+
+  } else { // We've already initialized this idle screen. Draw next frame!
+    display.clearDisplay();
+
+    // Draw each snowflake:
+    for (int8_t f = 0; f < NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo_bmp, LOGO_WIDTH, LOGO_HEIGHT, SSD1306_WHITE);
+    }
+
+    display.display(); // Show the display buffer on the screen
+    // delay(200);        // Pause for 1/10 second. Should we?
+
+    // Then update coordinates of each flake...
+    for (int8_t f = 0; f < NUMFLAKES; f++) {
+      icons[f][YPOS] += icons[f][DELTAY];
+      // If snowflake is off the bottom of the screen...
+      if (icons[f][YPOS] >= display.height()) {
+        // Reinitialize to a random position, just off the top
+        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+        icons[f][YPOS]   = -LOGO_HEIGHT;
+        icons[f][DELTAY] = random(1, 6);
+      }
+    }
+
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   last_interacted_time = 0;
-  idle_one = 0;
+  last_idle_change = 0;
+  idle_one = 1; // Display idle screen one first by default
   idle_two = 0;
   displayed_computer = 0;
   displayed_player = 0;
@@ -303,5 +293,5 @@ void setup() {
 
 void loop() {
   button = digitalRead(22); // Digital pin 22
-  display_idle_screen(OLED_timer, !button, 0, 1, 5); // test values for screens & comp_diff
+  display_idle_screen(OLED_timer, !button, 0, 5); // test values for screens & comp_diff
 }
