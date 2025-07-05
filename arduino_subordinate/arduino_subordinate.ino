a5acd1f9133b10e4f2a97497d6107e9b109cfbbe
AG2048 <45613281+AG2048@users.noreply.github.com>
2025-07-05 15:58:25 -0400

    adding print to arduino

--- arduino_subordinate/arduino_subordinate.ino
+++ arduino_subordinate/arduino_subordinate.ino
@@ -27,6 +27,7 @@ void setup() {
   }
   Wire.begin(SUBORDINATE_ADDR);
   Wire.onRequest(requestEvent);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -44,4 +45,6 @@ void requestEvent() {
   // Send as 2 bytes, LSB first
   Wire.write(value & 0xFF);        // Send LSB
   Wire.write((value >> 8) & 0xFF); // Send MSB
+  Serial.print("Printed Value: ");
+  Serial.println(value);
 }
