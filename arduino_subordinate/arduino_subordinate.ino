2d262401b86580e9d8c2fef4390c11d895d88dea
George Fan <georgefan112358@gmail.com>
2025-09-20 12:36:23 -0400

    Motor code to mega
    Just the constants, before modifying the I2C

--- arduino_subordinate/arduino_subordinate.ino
+++ arduino_subordinate/arduino_subordinate.ino
@@ -2,7 +2,6 @@
 
 #define SUBORDINATE_ADDR 8
 
-const uint8_t pins[10] = {31, 33, 35, 37, 39, 41, 43, 45, 47, 49};
 // Order of pins follow:
 /*
 enum {
@@ -19,11 +18,38 @@ enum {
   JOYSTICK_PINS_COUNT
 };
 */
-volatile uint16_t pinStates = 0;
+
+const uint8_t DPAD_PINS[10] = {31, 33, 35, 37, 39, 41, 43, 45, 47, 49};
+const uint8_t LIMIT_PINS[4] = {}; // x-, x+, y-, y+
+const uint8_t DIR_PINS[2] = {};   // x, y
+const uint8_t PUL_PINS[2] = {};   // x, y
+const uint8_t PICKER_PIN = ;
+
+Servo piece_picker;
+const float MM_PER_SQUARE = 66.7; // width of chessboard squares in mm
+const float GRAVEYARD_GAP = 10.0;   // gap between graveyard and chessboard in mm
+const uint8_t PICKER_ANGLE[2] {}; // up angle, down angle
+const int8_t BED_LEVEL_OFFSET[8][8] = {
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0}
+}; // x, y
+enum {
+  X_AXIS,
+  Y_AXIS,
+  XY_AXIS
+};
+
+int motor_coordinates[2] = {-(MM_PER_SQUARE*3 + GRAVEYARD_GAP), 0};
 
 void setup() {
   for (uint8_t i = 0; i < 10; i++) {
-    pinMode(pins[i], INPUT_PULLUP); // Set pins as input with pull-up resistors
+    pinMode(DPAD_PINS[i], INPUT_PULLUP); // Set pins as input with pull-up resistors
   }
   Wire.begin(SUBORDINATE_ADDR);
   Wire.onRequest(requestEvent);
@@ -38,7 +64,7 @@ void requestEvent() {
   // Read pin states and pack into 10 bits (LSB = pin 31)
   uint16_t value = 0;
   for (uint8_t i = 0; i < 10; i++) {
-    if (digitalRead(pins[i])) {
+    if (digitalRead(DPAD_PINS[i])) {
       value |= (1 << i);
     }
   }
